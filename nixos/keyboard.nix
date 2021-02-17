b996bc522c3fe51e456947e1661615a8b3ec51fd
Nolan Rigo <nolan@rigo.dev>
2021-02-17 16:34:08 +0100

    keyboard/ french special character binding

--- nixos/keyboard.nix
+++ nixos/keyboard.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   params = import ./params.nix;
@@ -6,5 +6,38 @@ in {
   services.xserver = {
     layout = "us";
     xkbVariant = "altgr-intl";
+    displayManager.sessionCommands = let
+      kbCustomLayout = pkgs.writeText "xkb-layout" ''
+        keycode 90 = eacute Eacute
+        keycode 87 = ecircumflex Ecircumflex
+        keycode 88 = egrave Egrave
+        keycode 84 = acircumflex Acircumflex
+        keycode 83 = agrave Agrave
+        keycode 89 = ccedilla Ccedilla
+        keycode 85 = ucircumflex Ucircumflex
+        keycode 79 = ugrave Ugrave
+        keycode 80 = icircumflex Icircumflex
+        keycode 125 = ocircumflex Ocircumflex
+      '';
+    in "${pkgs.xorg.xmodmap}/bin/xmodmap ${kbCustomLayout}";
+  };
+
+  home-manager.users."${params.username}" = {
+    # Activate numlock
+    xsession.numlock = {
+      enable = true;
+    };
+
+    /**
+     * FIXME: home-manager, by default, reset keyboard when it starts
+     * apply this change to keep our keyboard' updates applied
+     * see: https://github.com/nix-community/home-manager/issues/543#issuecomment-481469067
+     */
+    systemd.user = {
+      services = {
+        setxkbmap.Service.ExecStart = lib.mkForce "${pkgs.coreutils}/bin/true";
+      };
+    };
   };
 }
+
