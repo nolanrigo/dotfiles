291c6851fc9a62f1f13ab6e03b9056c3e81a447e
Nolan Rigo <nolan@rigo.dev>
2022-02-06 19:07:00 +0100

    gpu settings

--- nixos/gpu.nix
+++ nixos/gpu.nix
@@ -3,14 +3,25 @@
 {
   environment.systemPackages = [];
   services.xserver = {
-    videoDrivers = ["modesetting"];
-    useGlamor = true;
+    dpi = 96;
+    videoDrivers = ["nvidia"];
+    # useGlamor = true;
     exportConfiguration = true;
   };
 
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      prime = {
+        sync.enable = true;
+        nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0:2:0";
+      };
+    };
   };
 }
