77387b77c75eea0ce8ac2567532874e91cbb9554
Nolan Rigo <nolan@rigo.dev>
2022-02-06 19:06:37 +0100

    file: thunar -> ranger

--- nixos/explore.nix
+++ nixos/explore.nix
@@ -4,14 +4,22 @@ let
   params = import ./params.nix;
 in {
   home-manager.users."${params.username}" = {
-    # Thunar
+    # Ranger
     home.packages = with pkgs; [
-      xfce.thunar
-      xfce.thunar-archive-plugin
-      xfce.thunar-dropbox-plugin
-      xfce.thunar-volman
-      xfce.tumbler
+      ranger
+      mediainfo # for viewing information about media files
+      jq # for JSON files
+      odt2txt # for OpenDocument text files (odt, ods, odp and sxw)
+      poppler_utils # for textual pdf previews, "pdftoppm" to preview as image
+      bat # for syntax highlighting of code
     ];
+
+    programs.fish.shellAbbrs.r = "ranger";
+    home.sessionVariables.PAGER = "more";
+
+    xdg.configFile."ranger/rc.conf" = {
+      text = '''';
+    };
 
     # Autodetect removable devices
     services.udiskie.enable = true;
