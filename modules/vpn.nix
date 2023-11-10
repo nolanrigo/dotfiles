3ced4477b14e1876dfd45f193c26b429e80ef981
Nolan Rigo <nolan@rigo.dev>
2023-11-10 20:39:58 +0100

    protonvpn-import

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -1,38 +1,52 @@
 { config, pkgs, ... }: {
-  home-manager.users."${config.user.name}" = let 
+  home-manager.users."${config.user.name}" = let
     scripts = {
-      proton-import = pkgs.writeText "proton-vpn-import" ''
+      proton-import = pkgs.writeText "protonvpn-import" ''
         #!/usr/bin/env bash
+
+        CONFIG_PATH=$XDG_CONFIG_HOME/protonvpn/ovpn
+
+        # Zip path from $1
+        ZIP_PATH=$1
+        if [ -z "$ZIP_PATH" ]; then
+          echo "Usage: protonvpn-import <path to zip file>"
+          exit 1
+        fi
 
         read -s -p  "OpenVPN username: " USERNAME;
         echo "";
         read -s -p  "OpenVPN password: " PASSWORD;
         echo "";
 
-        for FILE in ~/vpns/protonvpn-*; do
-          NAME=$(basename $FILE);
+        # Reset from the previous import
+        rm -rf $CONFIG_PATH
+        for CONN in $(nmcli con show | rg protonvpn | awk '{print $1}'); do
+            nmcli con delete "$CONN"
+        done
+        # Create new empty directory
+        mkdir -p $CONFIG_PATH
 
-          # if the vpn connnection already exist, delete it
-          if nmcli connection show "$NAME" &> /dev/null; then
-            echo "Delete $NAME";
-            nmcli connection delete "$NAME";
-          fi
+        # Extraire et traiter les fichiers .ovpn
+        unzip -o "$ZIP_PATH" -d $CONFIG_PATH
+        for FILE in $CONFIG_PATH/*.ovpn; do
+            # Importer les connexions VPN
+            nmcli con import type openvpn file "$FILE"
 
-          # import vpn connection
-          echo "Import $FILE";
-          nmcli c import type openvpn file $FILE;
+            # Modifier le nom
+            ORIGINAL_NAME=$(basename "$FILE" .ovpn)
+            LANGUAGE=$(echo $ORIGINAL_NAME | sed -n 's/^\([^.]*\)\..*$/\1/p')
+            NEW_NAME="protonvpn-$LANGUAGE"
+            nmcli con modify "$ORIGINAL_NAME" connection.id "$NEW_NAME"
 
-          # set credentials for this connection
-          echo "Set username";
-          nmcli c mod $NAME vpn.user-name "$USERNAME";
-          echo "Set password";
-          nmcli c mod $NAME vpn.secrets "password=$PASSWORD";
+            # Set username and password for each VPN connection
+            nmcli con modify "$NEW_NAME" vpn.user-name "$USERNAME"
+            nmcli con modify "$NEW_NAME" +vpn.secrets "password=$PASSWORD"
         done
       '';
     };
   in {
     programs.fish.shellAbbrs = {
-      vpn-proton-import = "bash ${scripts.proton-import}";
+      protonvpn-import = "bash ${scripts.proton-import}";
     };
   };
 }
